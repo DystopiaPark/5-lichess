af9607b0e32951fc844d1ec29263a157e1dbb926
Filip <68464364+DystopiaPark@users.noreply.github.com>
2023-02-12 14:16:11 +0100

    update

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-let board = document.getElementById("b");
-
+let board = document.getElementById("bb");
 for (i = 1; i <= 64; i++) {
   if (i < 9 || (i > 16 && i < 25) || (i > 32 && i < 41) || (i > 48 && i < 57)) {
     if (i % 2 != 0) {
       let white = document.createElement("span");
-      white.style.backgroundColor = "lightgreen";
+      white.style.backgroundColor = "rgb(50, 48, 48)";
       white.textContent = "\u00A0";
       board.append(white);
     } else {
@@ -19,7 +18,7 @@ for (i = 1; i <= 64; i++) {
   } else {
     if (i % 2 == 0) {
       let white = document.createElement("span");
-      white.style.backgroundColor = "lightgreen";
+      white.style.backgroundColor = "rgb(50, 48, 48)";
       white.textContent = "\u00A0";
       board.append(white);
       if (i % 8 == 0) {
